0920f85340b45821a903e9ddf733fb602ade2dfe
ebisLab <ebaiden@techie.com>
2022-05-31 18:09:18 -0400

    deeper dive on useeffect dependency

--- src/containers/AppClass.js
+++ src/containers/AppClass.js
@@ -5,12 +5,13 @@ import ErrorBoundary from '../components/ErrorBoundary'
 const AppClass =()=> {
     const [data, setData]= useState([]);
     const [searchfield, setSearchField] =useState('')
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(res=>res.json())
         .then(users=> setData(users))
-    }, [])
+    }, [count])
     
     const onSearchChange = (event)=>{
         setSearchField(event.target.value) //this.state.searchField
@@ -24,6 +25,8 @@ const AppClass =()=> {
       <div>
           <h1>With App class</h1>
           <SearchBox onSearchChange={onSearchChange} />
+          <button onClick={()=>setCount(1+count)}>Click Me</button>
+          {count}
           <ErrorBoundary>
           <CardList data={filteredValues}/>
           </ErrorBoundary>
