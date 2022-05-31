344595f5493e9968dc0fba79844c7d8f96f3e342
ebisLab <ebaiden@techie.com>
2022-05-31 16:09:59 -0400

    converted to hooks

--- src/containers/AppClass.js
+++ src/containers/AppClass.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBox from '../components/SearchBox'
 import CardList from '../components/CardList'
 import ErrorBoundary from '../components/ErrorBoundary'
-export default class AppClass extends Component {
-    constructor(){
-        super() //calls constructor of component
-        this.state={
-            data: [], 
-            searchfield:''
-        }
+const AppClass =()=> {
+    const [data, setData]= useState([]);
+    const [searchfield, setSearchField] =useState('')
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(users=> setData(users))
+    }, [])
+    
+    const onSearchChange = (event)=>{
+        setSearchField(event.target.value) //this.state.searchField
     }
 
-    componentDidMount(){
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res=>res.json())
-.then(users=> this.setState({data:users}))
-    }
-
-    onSearchChange = (event)=>{
-        console.log(event);
-        this.setState({searchfield: event.target.value}) //this.state.searchField
-    }
-  render() {
-    const {data, searchfield} = this.state
     const filteredValues = data.filter(item=>{
         return item.name.toLowerCase().includes(searchfield.toLowerCase())
     })
@@ -30,11 +23,12 @@ fetch('https://jsonplaceholder.typicode.com/users')
      (
       <div>
           <h1>With App class</h1>
-          <SearchBox onSearchChange={this.onSearchChange} />
+          <SearchBox onSearchChange={onSearchChange} />
           <ErrorBoundary>
           <CardList data={filteredValues}/>
           </ErrorBoundary>
       </div>
     )
   }
-}
+
+  export default AppClass
