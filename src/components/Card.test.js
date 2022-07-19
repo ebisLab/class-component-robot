0edd5c35c9bd49d8e5a1b1bff7e9a53406e4a353
ebisLab <ebaiden@techie.com>
2022-07-19 12:02:44 -0400

    card test

---
+++ src/components/Card.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Card from "./Card";
+
+it("renders the Card component", () => {
+  render(<Card name="gio" />);
+  screen.getByText("gio");
+  expect(screen.getByText("gio")).toBeInTheDocument();
+});
+
+it("should take a snapshot", () => {
+  const { asFragment } = render(<Card name="hi ther" />);
+  expect(asFragment(<Card name="hi there" />)).toMatchSnapshot();
+});
